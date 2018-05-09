9a43847742fcd5adaa4baf44d468768b8681943f
tmichalowski <tomasz.michalowski@atos.net>
2018-05-09 07:17:49 +0200

    1

--- test_ssh.py
+++ test_ssh.py
@@ -4,7 +4,7 @@ from Queue import Queue
 locke1 = threading.Lock()
 q = Queue()
 
-#Check the login
+
 def check_hostname(host_name):
     with locke1:
         print ("Checking hostname :"+str(host_name)+" with " + threading.current_thread().name)
@@ -16,40 +16,33 @@ def check_hostname(host_name):
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
           ssh.connect(host_name, username='username',  timeout=5)
-          #print ("Success")
+
           file_success.write(str(host_name+"\n"))
           file_success.close()
           file_output.write("succes "+str(host_name+"\n"))
           file_output.close()
 
-          # printing output if required from remote machine
-          #stdin,stdout,stderr = ssh.exec_command("hostname&&uptime")
-          #for line in stdout.readlines():
-           # print (line.strip())
+
 
         except paramiko.SSHException:
-                # print ("error")
                 file_failed.write(str(host_name+"\n"))
                 file_failed.close()
                 file_output.write("failed "+str(host_name+"\n"))
                 file_output.close()
-                #quit()
-        except paramiko.ssh_exception.NoValidConnectionsError:
-                #print ("might be windows------------")
+				
+        except paramiko.ssh_exception.NoValidConnectionsError:)
                 file_output.write("failed "+str(host_name + "\n"))
                 file_output.close()
                 file_failed.write(str(host_name+"\n"))
                 file_failed.close()
-                #quit()
+				
         except socket.gaierror:
-          #print ("wrong hostname/dns************")
           file_output.write("error "+str(host_name+"\n"))
           file_output.close()
           file_error.write(str(host_name + "\n"))
           file_error.close()
 
         except socket.timeout:
-           #print ("No Ping %%%%%%%%%%%%")
            file_output.write("error "+str(host_name+"\n"))
            file_output.close()
            file_error.write(str(host_name + "\n"))
